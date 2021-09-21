d82bece820f52f71040a0a2079eed413accbf724
Arttu Linkinen <arttu.linkinen@gmail.com>
2021-09-21 14:48:46 +0300

    heroku

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 morgan.token('post', (req) => {
@@ -102,7 +104,7 @@ app.get('/info', (req, res) => {
 
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`)
 })
