1ddb1b67601fe08023ee82c88953d97d140741a8
Arttu Linkinen <arttu.linkinen@gmail.com>
2021-09-22 18:28:17 +0300

    tietokanta step 3-6

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const Person = require('./models/person')
+const { response } = require('express')
+const { countDocuments } = require('./models/person')
 const app = express()
 
 app.use(express.static('build'))
@@ -16,9 +18,14 @@ morgan.token('post', (req) => {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post'))
 
-const generatedId = () => {
-    const id = Math.floor(Math.random() * 100000)
-    return id
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({error: 'malformatted id'})
+    }   
+    next(error)   
 }
 
 app.post('/api/persons', (req, res) => {
@@ -53,37 +60,58 @@ app.get('/api/persons', (req, res) => {
     })
 })
 
-app.get('/api/persons/:id', (req, res) => {
+app.get('/api/persons/:id', (req, res, next) => {
     Person.findById(req.params.id)
     .then(person => {
-        res.json(person)
+        console.log(person)
+        if (person) {
+            res.json(person)
+        } else {
+            res.status(404).end()
+        }
     })
-    .catch((error) => {
-        res.status(404).send({ error: 'unknown id'})
-    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(person => person.id !== id)
+app.delete('/api/persons/:id', (req, res, next) => {
+    console.log(req.params.id)
+    Person.findByIdAndRemove(req.params.id)
+        .then(result => {
+            res.status(204).end()
+        })
+        .catch(error => next(error))
+})
 
-    res.status(204).end()
+app.put('/api/persons/:id', (req, res, next) => {
+    const body = req.body
+    
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
 
-
+    Person.findByIdAndUpdate(req.params.id, person, { new: true })
+        .then(updateNote => {
+            res.json(updateNote)
+        })
+        .catch(error => next(error))
 })
 
 app.get('/info', (req, res) => {
-    console.log('/info working')
-    const date = new Date()
-    res.send(
-        `<div>
-            <p>Phonebook has info for ${persons.length} people</p>
-            <p>${date}</p>
-        </div>`
-    )
+    Person.estimatedDocumentCount()
+        .then((countDocuments) => {
+            const date = new Date()
+                res.send(
+                    `<div>
+                        <p>Phonebook has info for ${countDocuments} people</p>
+                        <p>${date}</p>
+                    </div>`
+                    )
+        })
+        .catch(error => next(error))
 })
 
-
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
