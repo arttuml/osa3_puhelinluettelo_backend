34e37e0aa5f6e0f2d6e2a65e9223028769ce88ee
Arttu Linkinen <arttu.linkinen@gmail.com>
2021-09-22 14:29:13 +0300

    tietokanta step 1-2

--- index.js
+++ index.js
@@ -1,41 +1,20 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 const app = express()
 
 app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
 
+
 morgan.token('post', (req) => {
     return JSON.stringify(req.body)
 })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post'))
-
-
-let persons = [
-    {
-        id: 1,
-        name: "Arto Hellas",
-        number: "040-123456"
-    },
-    {
-        id: 2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523"
-    },
-    {
-        id: 3,
-        name: "Dan Abramov",
-        number: "12-43-234345"
-    },
-    {
-        id: 4,
-        name: "Mary Poppendick",
-        number: "39-23-6423122"
-    },
-]
 
 const generatedId = () => {
     const id = Math.floor(Math.random() * 100000)
@@ -44,43 +23,44 @@ const generatedId = () => {
 
 app.post('/api/persons', (req, res) => {
     const body = req.body
-    const nameUniqueTest = persons.find(p => p.name.toLocaleLowerCase() === body.name.toLocaleLowerCase())
+    // const nameUniqueTest = Person.find(p => p.name.toLocaleLowerCase() === body.name.toLocaleLowerCase())
 
-    if (!body.number || !body.name) {
-        return res.status(400).json({
-            error: 'content missing'
-        })
-    }
+    // if (!body.number || !body.name) {
+    //     return res.status(400).json({
+    //         error: 'content missing'
+    //     })
+    // }
 
-    if (nameUniqueTest) {
-        return res.status(400).json({
-            error: 'name must be unique'
-        })
-    }
+    // if (nameUniqueTest) {
+    //     return res.status(400).json({
+    //         error: 'name must be unique'
+    //     })
+    // }
 
-    const person = {
-        id: generatedId(),
+    const person = new Person({
         name: body.name,
         number: body.number
-    }
+    })
 
-    persons = persons.concat(person)
-    res.json(person)
+    person.save().then(savedNote => {
+        res.json(savedNote)
+    })
 })
 
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+    Person.find({}).then(persons => {
+        res.json(persons)
+    })
 })
 
 app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const person = persons.find(p => p.id === id)
-    
-    if (person) {
+    Person.findById(req.params.id)
+    .then(person => {
         res.json(person)
-    } else {
-        res.status(404).end()
-    }
+    })
+    .catch((error) => {
+        res.status(404).send({ error: 'unknown id'})
+    })
 })
 
 app.delete('/api/persons/:id', (req, res) => {
